ded944b6a97ad9ea866fea1f5359c16ad9353c4f
Sejal Mane <sejalofficial20@gmail.com>
2025-11-26 13:42:39 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
+import os
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Render provides the port in the PORT environment variable
+    port = int(os.environ.get("PORT", 5000))
+
+    # Bind to 0.0.0.0 so Render can access your service
+    app.run(host="0.0.0.0", port=port)
